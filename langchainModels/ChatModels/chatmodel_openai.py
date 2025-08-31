d3492387a7b764eeffbecccae25a4e9025b1989d
HardikDudeja <hardik.dudeja39@gmail.com>
2025-08-31 23:54:04 +0530

    start langchain

---
+++ langchainModels/ChatModels/chatmodel_openai.py
@@ -0,0 +1,13 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-5-nano")
+
+result = model.invoke("What is the capital of India?")
+
+print(result)
+
+
+
